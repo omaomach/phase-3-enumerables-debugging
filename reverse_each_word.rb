8cdb52a06612c52e1b271cfa546101ea48b8c44f
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-10-18 19:40:54 +0300

    completed phase 3 enumerables debugging

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,37 @@
-# Write your code here
+require 'pry'
+
+# Reverse words in a sentence
+# 1. Split the string on "" to get access to each word in the sentence.
+
+# def reverse_each_word sentence
+#     words = sentence.split
+#     # 2. Reverse each word and add it to a new array
+#     reversed_words = []
+#     words.each do |word|
+#         reversed_words << word.reverse
+#     end
+    # 3. Join the array of words on "" to create one string
+#     reversed_words.join(" ")
+# end
+
+# def reverse_each_word sentence
+#     words = sentence.split
+#     reversed_words = words.map do |word|
+#         word.reverse
+#     end
+#     reversed_words.join(" ")
+# end
+
+# def reverse_each_word sentence
+#     words = sentence.split
+#     reversed_words = words.map(&:reverse)
+#     reversed_words.join(" ")
+# end
+
+def reverse_each_word sentence
+    sentence.split.map(&:reverse).join(" ")
+end
+
+binding.pry
+
+reverse_each_word("Software Engineer Joash Omao")
